5b94ecbeadf0f8feb6fe5ebb330b0cbb7e6971d8
ratnadeep007 <ratnadeepofghato@gmail.com>
2021-06-27 12:42:54 +0530

    added workflow and --version flag

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"manifest_creator/cli"
+	"os"
 )
 
 func main() {
+	if err := os.Args[1:]; err != nil {
+		if os.Args[1] == "--version" {
+			fmt.Println("Manifestor Alpha v1")
+			os.Exit(0)
+		}
+	}
+
 	cli.Run()
 }
