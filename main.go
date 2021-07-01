f4f96a7adf30f9762f9ebdebc17fb477bfce2dfa
Ratnadeep Bhattcharyya <ratnadeep.bhattacharyya@cura.tech>
2021-07-01 10:10:34 +0530

    os args bug fix

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
-	if err := os.Args[1:]; err != nil {
-		if os.Args[1] == "--version" {
-			fmt.Println("Manifestor Alpha v1")
-			os.Exit(0)
+	fmt.Println(len(os.Args))
+	if len(os.Args) > 1 {
+		if err := os.Args[1:]; err != nil {
+			if os.Args[1] == "--version" {
+				fmt.Println("Manifestor Alpha v1")
+				os.Exit(0)
+			}
 		}
 	}
 
